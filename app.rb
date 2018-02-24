4ed9993abbf4917583c57efce291aabb39b07fe4
Miwa <paintfx@gmail.com>
2018-02-24 15:29:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -27,17 +27,17 @@ class App < Sinatra::Base
 
   get '/:operation/:number1/:number2' do
     @operation = params[:operation]
-    @num1 = params[:number1].to_i
+    num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
 
     if @operation == "add"
-      result = @num1 + @num2
+      result = num1 + @num2
     elsif operation == "subtract"
-      result = @num2 - @num1
+      result = @num2 - num1
     elsif operation == "multiply"
-      result = @num1 * @num2
+      result = num1 * @num2
     elsif operation == "divide"
-      result = @num1 / @num2
+      result = num1 / @num2
     end
     result.to_s
   end
